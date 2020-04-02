61592db7f6b09e951abe3cf9b85e251f6d49032c
Edson Coura <edsoncoura@yahoo.com.br>
2020-04-02 17:34:44 -0300

    aula06 - vetores

---
+++ repos/CursoCSharp2020/Aula06/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Estudante[] vect = new Estudante[10];
+
+
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Aluguel #" + i);
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("===================================================");
+                Console.WriteLine("Quartos Vagos");
+                for (int a = 0; a < 10; a++)
+                  if (vect[a] == null)
+                    {
+                        Console.WriteLine(i + ": " + vect[i]);
+                    }
+                Console.WriteLine("===================================================");
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Quartos Ocupados:");
+            for (int i = 0; i < 10; i++) 
+                if (vect[i] != null)
+            {
+                Console.WriteLine(i + ": " + vect[i]);
+            }
+
+        }
+    }
+}

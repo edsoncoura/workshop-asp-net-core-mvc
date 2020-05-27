730db1bd6469a9a5e2562f1eb9d02941a50a8ca7
Edson Coura <edsoncoura@yahoo.com.br>
2020-05-27 15:51:01 -0300

    Seeding Service

--- SalesWebMvc/SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/SalesWebMvc/Models/SalesRecord.cs
@@ -5,26 +5,25 @@ namespace SalesWebMvc.Models
 {
     public class SalesRecord
     {
+       
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
-        public int MyProperty { get; set; }
-        public SaleStatus Status { get; set; }
+         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
 
         public SalesRecord()
         {
         }
 
-        public SalesRecord(int id, DateTime date, double amount, int myProperty, SaleStatus status, 
-                           Seller seller)
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
         {
             Id = id;
             Date = date;
             Amount = amount;
-            MyProperty = myProperty;
             Status = status;
             Seller = seller;
         }
+
     }
 }
